eda7733f075882e192706d7d875747467349fbd3
Raymundo Mateos <rayma9829@gmail.com>
2023-02-09 01:02:04 -0600

    [ADD] Funcionalidad para mostrar información del codigo de la tecla presionada

--- public/index.js
+++ public/index.js
@@ -1,9 +1,11 @@
 let emptyText = document.querySelector('div.empty');
 let infoDiv = document.querySelector('div.info');
 let key = document.querySelector('div.key');
+let code = document.querySelector('div.code');
 
 document.addEventListener('keydown', (e) => {
     emptyText.classList.add('dispose');
     infoDiv.classList.remove('dispose');
-    key.innerHTML = `<p class="keyText">${e.key}</p>`;
+    key.innerHTML = `<p class="infText">${e.key}</p>`;
+    code.innerHTML = `<p class="infText">${e.code}</p>`;
 });
