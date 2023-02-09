bd9b392a24d9ecc255b6ac5a62b78e23cc9f9b31
Raymundo Mateos <rayma9829@gmail.com>
2023-02-09 02:13:55 -0600

    [ADD] Set time out para esperar a que desaparezca el texto inicial y asegurarse que se hace la transición

--- public/index.js
+++ public/index.js
@@ -6,6 +6,14 @@ let keyCode = document.querySelector('.infText.keyCode');
 let shiftKey = document.querySelector('.infText.shiftKey');
 let ctrlKey = document.querySelector('.infText.ctrlKey');
 let altKey = document.querySelector('.infText.altKey');
+let infFields = document.querySelectorAll('.inf');
+
+infFields.forEach(field => {
+    field.addEventListener('transitionend', (e) => {
+        if(e.propertyName !== 'border-right-width') return;
+        e.target.classList.remove('active');
+    });
+})
 
 document.addEventListener('keydown', (e) => {
     emptyText.classList.add('dispose');
@@ -16,4 +24,9 @@ document.addEventListener('keydown', (e) => {
     shiftKey.innerHTML = e.shiftKey;
     ctrlKey.innerHTML = e.ctrlKey;
     altKey.innerHTML = e.altKey;
+    setTimeout(() => {
+        infFields.forEach(field => {
+            field.classList.add('active');
+        });
+    }, 100);
 });
