6e5fb74d8ac6271e0f054d37dbef8c67c2169b95
Raymundo Mateos <rayma9829@gmail.com>
2023-02-09 00:52:07 -0600

    [ADD] Función para añadir parrafo para mostrar la tecla presionada

--- public/index.js
+++ public/index.js
@@ -1,7 +1,9 @@
 let emptyText = document.querySelector('div.empty');
 let infoDiv = document.querySelector('div.info');
+let key = document.querySelector('div.key');
 
 document.addEventListener('keydown', (e) => {
     emptyText.classList.add('dispose');
     infoDiv.classList.remove('dispose');
+    key.innerHTML = `<p class="keyText">${e.key}</p>`;
 });
