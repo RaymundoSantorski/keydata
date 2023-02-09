192eefac02ea6fc3aa990504081da7e4e1582818
Raymundo Mateos <rayma9829@gmail.com>
2023-02-09 01:30:24 -0600

    [EDIT] Ahora se selecciona directamente la etiqueta kbd para cambiar su texto y asi no generar toda la etiqueta <p> cada que se presiona una nueva tecla

--- public/index.js
+++ public/index.js
@@ -1,11 +1,14 @@
 let emptyText = document.querySelector('div.empty');
 let infoDiv = document.querySelector('div.info');
-let key = document.querySelector('div.key');
-let code = document.querySelector('div.code');
+let key = document.querySelector('.infText.key');
+let code = document.querySelector('.infText.code');
+let keyCode = document.querySelector('.infText.keyCode');
+
 
 document.addEventListener('keydown', (e) => {
     emptyText.classList.add('dispose');
     infoDiv.classList.remove('dispose');
-    key.innerHTML = `<p class="infText">${e.key}</p>`;
-    code.innerHTML = `<p class="infText">${e.code}</p>`;
+    key.innerHTML = e.key;
+    code.innerHTML = e.code;
+    keyCode.innerHTML = e.keyCode;
 });
