1780efb7e6b068ccb4a0794de31281cd3715f614
Raymundo Mateos <rayma9829@gmail.com>
2023-02-09 00:23:51 -0600

    [ADD] Funcionalidad para mostrar el panel de infomación luego de ocultar el texto inicial

--- public/index.js
+++ public/index.js
@@ -1,5 +1,7 @@
 let emptyText = document.querySelector('div.empty');
+let infoDiv = document.querySelector('div.info');
 
 document.addEventListener('keydown', (e) => {
     emptyText.classList.add('dispose');
+    infoDiv.classList.remove('dispose');
 });
